404c8f32f16cb2ad61552b130ed2b6a14c24a9cb
brandonT2002 <3060571900304@ingenieria.usac.edu.gt>
2023-04-20 16:27:59 -0600

    separate the message using regular expressions

--- Backend/Read.py
+++ Backend/Read.py
@@ -1,4 +1,5 @@
 from xml.dom import minidom
+import re
 
 class Read:
     def readProfiles(self,content):
@@ -30,12 +31,20 @@ class Read:
         message = messages.getElementsByTagName('mensaje')
 
         print('--- Mensajes ---')
-        for msg in message:
-            msg = msg.firstChild.data
-            print(msg)
+        for text in message:
+            text = text.firstChild.data
+            placeDate = re.search(r"Lugar y Fecha: (.+)", text).group(1)
+            user = re.search(r"Usuario: (.+)", text).group(1)
+            socialN = re.search(r"Red social: (.+)", text).group(1)
+            messg = re.split(r"Lugar y Fecha: .+\nUsuario: .+\nRed social: .+\n\n", text)[1]
+
+            print("Lugar y Fecha:", placeDate)
+            print("Usuario:", user)
+            print("Red social:", socialN)
+            print("Mensaje:", messg)
 
 
 # Read().readProfiles(open('./Perfiles.xml',encoding='utf-8').read())
 read = Read()
-read.readProfiles(open('./Perfiles.xml',encoding='utf-8').read())
+# read.readProfiles(open('./Perfiles.xml',encoding='utf-8').read())
 read.readMessage(open('./Mensajes.xml',encoding='utf-8').read())
