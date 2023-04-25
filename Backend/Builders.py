075e4bb051477ab489c1d3b2db1a5d3b2b65d2f1
brandonT2002 <3060571900304@ingenieria.usac.edu.gt>
2023-04-25 16:39:59 -0600

    objects are implemented

--- Backend/Builders.py
+++ Backend/Builders.py
@@ -2,3 +2,15 @@ class Profile:
     def __init__(self,name):
         self.name = name
         self.words = []
+
+class User:
+    def __init__(self,user):
+        self.user = user
+        self.messages = []
+
+class Message:
+    def __init__(self,place,date,time,content):
+        self.place = place
+        self.date = date
+        self.time = time
+        self.content = content
