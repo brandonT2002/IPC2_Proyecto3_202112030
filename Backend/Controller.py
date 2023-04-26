d5fb1f2123bbc462145a95d1d61a1a12d89ac580
brandonT2002 <3060571900304@ingenieria.usac.edu.gt>
2023-04-25 22:11:45 -0600

    show discarded words

--- Backend/Controller.py
+++ Backend/Controller.py
@@ -11,12 +11,19 @@ class Controller:
         self.rd.readProfiles(open('./Perfiles.xml',encoding='utf-8').read())
 
     def viewProfiles(self):
+        print('-----Perfiles-----')
         for profile in self.profiles:
             print(f'---{profile.name}---')
             for word in profile.words:
                 print(word)
             print()
 
+    def viewDiscarded(self):
+        print('-----Descartados-----')
+        for word in self.discarded:
+            print(word)
+
 ctrl = Controller()
 ctrl.readProfiles()
-ctrl.viewProfiles()
+# ctrl.viewProfiles()
+# ctrl.viewDiscarded()
